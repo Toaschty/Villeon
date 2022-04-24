1217f80976f4ea8f2397026e0df472ddbe1895ed
Reisacher <oliver.reisacher@hs-weingarten.de>
2022-04-24 18:30:28 +0200

    Small adjustments

--- Villeon/Villeon/Game.cs
+++ Villeon/Villeon/Game.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Mathematics;
