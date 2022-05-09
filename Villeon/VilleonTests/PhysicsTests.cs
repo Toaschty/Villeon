ae47eb1a157f0fa29115bb472dd4c4ce0504bed8
Reisacher <oliver.reisacher@hs-weingarten.de>
2022-05-09 22:30:30 +0200

    Unit-Test clean-up

--- Villeon/VilleonTests/PhysicsTests.cs
+++ Villeon/VilleonTests/PhysicsTests.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenTK.Mathematics;
 using Villeon;
 using Villeon.Components;
-using OpenTK.Mathematics;
 using Villeon.Systems;
 
 namespace VilleonTests
@@ -14,20 +14,19 @@ namespace VilleonTests
     [TestClass]
     public class PhysicsTests
     {
-        Scene testScene;
+        private Scene _testScene = new Scene("Testing");
 
         [TestInitialize]
         public void SetUp()
         {
             TypeRegistry.Init();
 
-            testScene = new Scene("Testing");
-            testScene.AddSystem(new PhysicsSystem("Physics"));
-            testScene.AddSystem(new CollisionSystem("Collision"));
+            _testScene.AddSystem(new PhysicsSystem("Physics"));
+            _testScene.AddSystem(new CollisionSystem("Collision"));
         }
 
         [TestMethod]
-        public void stopEntityIfCollidesWithFloor()
+        public void StopEntityIfCollidesWithFloor()
         {
             /*
                     PPP
@@ -50,25 +49,25 @@ namespace VilleonTests
             floor.AddComponent(new Collider(new Vector2(0, 0), new Vector2(-5, 0), 10, 1));
 
             // Setup Test-Scene
-            testScene.AddEntity(physicEntity);
-            testScene.AddEntity(floor);
+            _testScene.AddEntity(physicEntity);
+            _testScene.AddEntity(floor);
 
             // Update the PhysicsSystem 10 times with 0.1f time between updates
             for (int i = 0; i < 10; i++)
             {
-                testScene.Update(0.1f);
+                _testScene.Update(0.1f);
             }
 
             // -12 equals the default Y velocity a entity has when standing still with an delta time of 0.1f
             Assert.AreEqual(-12, physicEntity.GetComponent<Physics>().Velocity.Y);
 
             // Clean up
-            testScene.RemoveEntity(physicEntity);
-            testScene.RemoveEntity(floor);
+            _testScene.RemoveEntity(physicEntity);
+            _testScene.RemoveEntity(floor);
         }
 
         [TestMethod]
-        public void stopEntityIfCollidesWithCeiling()
+        public void StopEntityIfCollidesWithCeiling()
         {
             /*
             ###################
@@ -91,13 +90,13 @@ namespace VilleonTests
             ceiling.AddComponent(new Collider(new Vector2(0, 0), new Vector2(-5, 10), 10, 1));
 
             // Setup Test-Scene
-            testScene.AddEntity(physicEntity);
-            testScene.AddEntity(ceiling);
+            _testScene.AddEntity(physicEntity);
+            _testScene.AddEntity(ceiling);
 
             // Update the PhysicsSystem 10 times with 0.1f time between updates
             for (int i = 0; i < 10; i++)
             {
-                testScene.Update(0.1f);
+                _testScene.Update(0.1f);
 
                 // Set acceleration to +120 to "invert" Gravity after each update
                 physicEntity.GetComponent<Physics>().Acceleration = new Vector2(0, 120f);
@@ -107,12 +106,12 @@ namespace VilleonTests
             Assert.AreEqual(12, physicEntity.GetComponent<Physics>().Velocity.Y);
 
             // Clean up
-            testScene.RemoveEntity(physicEntity);
-            testScene.RemoveEntity(ceiling);
+            _testScene.RemoveEntity(physicEntity);
+            _testScene.RemoveEntity(ceiling);
         }
 
         [TestMethod]
-        public void stopEntityIfCollidesWithLeftWall()
+        public void StopEntityIfCollidesWithLeftWall()
         {
             /*
             ##
@@ -136,14 +135,14 @@ namespace VilleonTests
             wall.AddComponent(new Collider(new Vector2(0, 0), new Vector2(-25, 1), 1, 10));
 
             // Setup Test-Scene
-            testScene.AddEntity(physicEntity);
-            testScene.AddEntity(floor);
-            testScene.AddEntity(wall);
+            _testScene.AddEntity(physicEntity);
+            _testScene.AddEntity(floor);
+            _testScene.AddEntity(wall);
 
             // Update the PhysicsSystem 50 times with 0.1f time between updates
             for (int i = 0; i < 50; i++)
             {
-                testScene.Update(0.1f);
+                _testScene.Update(0.1f);
 
                 // Add acceleration of -80f in x direction each update
                 physicEntity.GetComponent<Physics>().Acceleration += new Vector2(-80f, 60f);
@@ -153,13 +152,13 @@ namespace VilleonTests
             Assert.AreEqual(-8, physicEntity.GetComponent<Physics>().Velocity.X);
 
             // Clean up
-            testScene.RemoveEntity(physicEntity);
-            testScene.RemoveEntity(floor);
-            testScene.RemoveEntity(wall);
+            _testScene.RemoveEntity(physicEntity);
+            _testScene.RemoveEntity(floor);
+            _testScene.RemoveEntity(wall);
         }
 
         [TestMethod]
-        public void stopEntityIfCollidesWithRightWall()
+        public void StopEntityIfCollidesWithRightWall()
         {
             /*
                              ##
@@ -183,14 +182,14 @@ namespace VilleonTests
             wall.AddComponent(new Collider(new Vector2(0, 0), new Vector2(25, 1), 1, 10));
 
             // Setup Test-Scene
-            testScene.AddEntity(physicEntity);
-            testScene.AddEntity(floor);
-            testScene.AddEntity(wall);
+            _testScene.AddEntity(physicEntity);
+            _testScene.AddEntity(floor);
+            _testScene.AddEntity(wall);
 
             // Update the PhysicsSystem 50 times with 0.1f time between updates
             for (int i = 0; i < 50; i++)
             {
-                testScene.Update(0.1f);
+                _testScene.Update(0.1f);
 
                 // Add acceleration of -80f in x direction each update
                 physicEntity.GetComponent<Physics>().Acceleration += new Vector2(80f, 60f);
@@ -200,9 +199,9 @@ namespace VilleonTests
             Assert.AreEqual(8, physicEntity.GetComponent<Physics>().Velocity.X);
 
             // Clean up
-            testScene.RemoveEntity(physicEntity);
-            testScene.RemoveEntity(floor);
-            testScene.RemoveEntity(wall);
+            _testScene.RemoveEntity(physicEntity);
+            _testScene.RemoveEntity(floor);
+            _testScene.RemoveEntity(wall);
         }
     }
 }
