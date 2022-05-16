8d0c6fb5e57247227dc16c91fe479e9def20a001
Marvin Pucknat <marvin.pucknat@hs-weingarten.de>
2022-05-16 21:11:06 +0200

    Added new collision tests

--- Villeon/VilleonTests/CollisionTests.cs
+++ Villeon/VilleonTests/CollisionTests.cs
@@ -12,131 +12,299 @@ namespace VilleonTests
     [TestClass]
     public class CollisionTests
     {
+        /*
+            How the clean collision works:
+            P = Player | E = enemy
+
+            PP
+            PP (Pos = 2)
+
+            EE
+            EE (Pos = 0)
+
+            Player now moves down onto the enemy: Player pos - 2
+
+            PP
+            EE (player and enemy pos are the same = 0)
+
+            The player will now be set on top of the enemy:
+
+            PP
+            PP (Pos = 1)
+            EE
+            EE (Pos = 0)
+       */
 
         [TestMethod]
-        public void CollisionUP()
+        public void CleanCollisionUp()
         {
             TypeRegistry.Init();
             CollisionSystem collisionSystem = new ("collisionSystem");
 
-            Vector2 start_pos_enemy = new Vector2(10, 10);
-            Vector2 start_pos_player = new Vector2(10, 11);
+            Entity enemy = new Entity(new Transform(new Vector2(0, 0), 1.0f, 0.0f), "Enemy");
+            enemy.AddComponent(new Collider(Vector2.Zero, new Vector2(0, 0), 1f, 1f));
 
-            // Collision Up
-            Entity enemy = new Entity(new Transform(start_pos_enemy, 1.0f, 0.0f), "enemy");
-            Entity player = new Entity(new Transform(start_pos_player, 1.0f, 0.0f), "Player");
+            Entity player = new Entity(new Transform(new Vector2(0, 2), 1.0f, 0.0f), "Player");
+            player.AddComponent(new Collider(Vector2.Zero, new Vector2(0, 2), 1f, 1f));
 
-            enemy.AddComponent(new Collider(Vector2.Zero, start_pos_enemy, 0.5f, 0.5f));
-            player.AddComponent(new Collider(Vector2.Zero, start_pos_player, 0.5f, 0.5f));
-
+            // Add every entity to the collisionsystem
             collisionSystem.Entities.Add(enemy);
             collisionSystem.Entities.Add(player);
 
-            // Act
-            collisionSystem.Update(0);
-            player.GetComponent<Transform>().Position -= new Vector2(0, 1);
+            // Move the player and then make the collision detection
+            player.GetComponent<Transform>().Position -= new Vector2(0, 2);
             collisionSystem.Update(0);
 
-            // Assert
-            Assert.AreEqual(new Vector2(10, 10.5f), player.GetComponent<Transform>().Position);
+            Assert.AreEqual(new Vector2(0, 1), player.GetComponent<Transform>().Position);
         }
 
         [TestMethod]
-        public void CollisionDown()
+        public void CleanCollisionDown()
         {
-            // Test when both player and enemy are going to the same point one of them should be set back to his start position
             TypeRegistry.Init();
             CollisionSystem collisionSystem = new ("collisionSystem");
 
-            Vector2 start_pos_enemy = new Vector2(10, 10);
-            Vector2 start_pos_player = new Vector2(10, 9);
+            Vector2 enemyStartPos = new Vector2(0, 2);
+            Vector2 playerStartPos = new Vector2(0, 0);
 
-            Entity enemy = new Entity(new Transform(start_pos_enemy, 1.0f, 0.0f), "enemy");
-            Entity player = new Entity(new Transform(start_pos_player, 1.0f, 0.0f), "Player");
+            Entity enemy = new Entity(new Transform(enemyStartPos, 1.0f, 0.0f), "enemy");
+            enemy.AddComponent(new Collider(Vector2.Zero, enemyStartPos, 1f, 1f));
 
-            enemy.AddComponent(new Collider(Vector2.Zero, start_pos_enemy, 0.5f, 0.5f));
-            player.AddComponent(new Collider(Vector2.Zero, start_pos_player, 0.5f, 0.5f));
+            Entity player = new Entity(new Transform(playerStartPos, 1.0f, 0.0f), "Player");
+            player.AddComponent(new Collider(Vector2.Zero, playerStartPos, 1f, 1f));
 
             collisionSystem.Entities.Add(enemy);
             collisionSystem.Entities.Add(player);
 
+            player.GetComponent<Transform>().Position += new Vector2(0, 2);
             collisionSystem.Update(0);
-            player.GetComponent<Transform>().Position += new Vector2(0, 1);
+
+            Assert.AreEqual(new Vector2(0, 1), player.GetComponent<Transform>().Position);
+        }
+
+        [TestMethod]
+        public void CleanCollisionRight()
+        {
+            TypeRegistry.Init();
+            CollisionSystem collisionSystem = new ("collisionSystem");
+
+            Vector2 enemyStartPos = new Vector2(0, 0);
+            Vector2 playerStartPos = new Vector2(2, 0);
+
+            Entity enemy = new Entity(new Transform(enemyStartPos, 1.0f, 0.0f), "Enemy");
+            enemy.AddComponent(new Collider(Vector2.Zero, enemyStartPos, 1f, 1f));
+
+            Entity player = new Entity(new Transform(playerStartPos, 1.0f, 0.0f), "Player");
+            player.AddComponent(new Collider(Vector2.Zero, playerStartPos, 1f, 1f));
+
+            collisionSystem.Entities.Add(enemy);
+            collisionSystem.Entities.Add(player);
+
+            player.GetComponent<Transform>().Position -= new Vector2(2, 0);
+            collisionSystem.Update(0);
+
+            Assert.AreEqual(new Vector2(1, 0), player.GetComponent<Transform>().Position);
+        }
+
+        [TestMethod]
+        public void CleanCollisionLeft()
+        {
+            TypeRegistry.Init();
+            CollisionSystem collisionSystem = new ("collisionSystem");
+
+            Vector2 enemyStartPos = new Vector2(2, 0);
+            Vector2 playerStartPos = new Vector2(0, 0);
+
+            Entity enemy = new Entity(new Transform(enemyStartPos, 1.0f, 0.0f), "Enemy");
+            enemy.AddComponent(new Collider(Vector2.Zero, enemyStartPos, 1f, 1f));
+
+            Entity player = new Entity(new Transform(playerStartPos, 1.0f, 0.0f), "Player");
+            player.AddComponent(new Collider(Vector2.Zero, playerStartPos, 1f, 1f));
+
+            collisionSystem.Entities.Add(enemy);
+            collisionSystem.Entities.Add(player);
+
+            player.GetComponent<Transform>().Position += new Vector2(2, 0);
+            collisionSystem.Update(0);
+
+            Assert.AreEqual(new Vector2(1, 0), player.GetComponent<Transform>().Position);
+        }
+
+        /*
+            How the dirty collision work:
+            P = Player | E = enemy
+
+            EE
+            EE (Pos = 2)
+
+               (Here is 0)
+
+            PP
+            PP (Pos = -2)
+
+            Both the player and the enemy are moving to 0 and collide there.
+            Here now is important that it depends on which order
+            the ENEMY and the PLAYER are registered to the collision system.
+            In this example first the enemy is registered and then the player.
+            The first registered element is the first one to be checked for collision.
+
+            EE
+            PP (player and enemy pos are the same = 0)
+
+            Enemey and the player are having the same position
+            -> Collision detected: enemy is going to be reset to his last position
+
+            EE
+            EE (Pos = 2)
+
+            PP
+            PP (Pos = 0)
+        */
+
+        [TestMethod]
+        public void DirtyCollisionUp()
+        {
+            TypeRegistry.Init();
+            CollisionSystem collisionSystem = new ("collisionSystem");
+
+            Vector2 start_pos_enemy = new Vector2(0, 2);
+            Vector2 start_pos_player = new Vector2(0, -2);
+
+            Entity enemy = new Entity(new Transform(start_pos_enemy, 1.0f, 0.0f), "Enemy");
+            enemy.AddComponent(new Collider(Vector2.Zero, start_pos_enemy, 1f, 1f));
+
+            Entity player = new Entity(new Transform(start_pos_player, 1.0f, 0.0f), "Player");
+            player.AddComponent(new Collider(Vector2.Zero, start_pos_player, 1f, 1f));
+
+            collisionSystem.Entities.Add(enemy);
+            collisionSystem.Entities.Add(player);
+
+            enemy.GetComponent<Transform>().Position -= new Vector2(0, 2);
+            player.GetComponent<Transform>().Position += new Vector2(0, 2);
+
             collisionSystem.Update(0);
 
             Assert.AreEqual(start_pos_enemy, enemy.GetComponent<Transform>().Position);
         }
 
         [TestMethod]
-        public void CollisionRight()
+        public void DirtyCollisionDown()
         {
-            // Test when both player and enemy are going to the same point one of them should be set back to his start position
             TypeRegistry.Init();
             CollisionSystem collisionSystem = new ("collisionSystem");
 
-            Vector2 start_pos_enemy = new Vector2(11, 10);
-            Vector2 start_pos_player = new Vector2(9, 10);
+            Vector2 start_pos_enemy = new Vector2(0, 2);
+            Vector2 start_pos_player = new Vector2(0, -2);
 
-            Entity enemy = new Entity(new Transform(start_pos_enemy, 1.0f, 0.0f), "enemy");
+            Entity enemy = new Entity(new Transform(start_pos_enemy, 1.0f, 0.0f), "Enemy");
+            enemy.AddComponent(new Collider(Vector2.Zero, start_pos_enemy, 1f, 1f));
+
             Entity player = new Entity(new Transform(start_pos_player, 1.0f, 0.0f), "Player");
+            player.AddComponent(new Collider(Vector2.Zero, start_pos_player, 1f, 1f));
 
-            enemy.AddComponent(new Collider(Vector2.Zero, start_pos_enemy, 0.5f, 0.5f));
-            player.AddComponent(new Collider(Vector2.Zero, start_pos_player, 0.5f, 0.5f));
+            collisionSystem.Entities.Add(player);
+            collisionSystem.Entities.Add(enemy);
+
+            enemy.GetComponent<Transform>().Position -= new Vector2(0, 2);
+            player.GetComponent<Transform>().Position += new Vector2(0, 2);
+
+            collisionSystem.Update(0);
+
+            Assert.AreEqual(start_pos_player, player.GetComponent<Transform>().Position);
+        }
+
+        [TestMethod]
+        public void DirtyCollisionRight()
+        {
+            TypeRegistry.Init();
+            CollisionSystem collisionSystem = new ("collisionSystem");
+
+            Vector2 start_pos_enemy = new Vector2(2, 0);
+            Vector2 start_pos_player = new Vector2(-2, 0);
+
+            Entity enemy = new Entity(new Transform(start_pos_enemy, 1.0f, 0.0f), "Enemy");
+            enemy.AddComponent(new Collider(Vector2.Zero, start_pos_enemy, 1f, 1f));
+
+            Entity player = new Entity(new Transform(start_pos_player, 1.0f, 0.0f), "Player");
+            player.AddComponent(new Collider(Vector2.Zero, start_pos_player, 1f, 1f));
 
             collisionSystem.Entities.Add(enemy);
             collisionSystem.Entities.Add(player);
 
-            collisionSystem.Update(0);
-            enemy.GetComponent<Transform>().Position += new Vector2(-1, 0);
-            player.GetComponent<Transform>().Position += new Vector2(1, 0);
+            enemy.GetComponent<Transform>().Position -= new Vector2(2, 0);
+            player.GetComponent<Transform>().Position += new Vector2(2, 0);
+
             collisionSystem.Update(0);
 
             Assert.AreEqual(start_pos_enemy, enemy.GetComponent<Transform>().Position);
         }
 
         [TestMethod]
-        public void CollisionLeft()
+        public void DirtyCollisionLeft()
         {
-            // Test when both player and enemy are going to the same point one of them should be set back to his start position
             TypeRegistry.Init();
             CollisionSystem collisionSystem = new ("collisionSystem");
 
-            Vector2 start_pos_enemy = new Vector2(11, 10);
-            Vector2 start_pos_player = new Vector2(9, 10);
+            Vector2 start_pos_enemy = new Vector2(2, 0);
+            Vector2 start_pos_player = new Vector2(-2, 0);
 
-            Entity enemy = new Entity(new Transform(start_pos_enemy, 1.0f, 0.0f), "enemy");
+            Entity enemy = new Entity(new Transform(start_pos_enemy, 1.0f, 0.0f), "Enemy");
+            enemy.AddComponent(new Collider(Vector2.Zero, start_pos_enemy, 1f, 1f));
+
             Entity player = new Entity(new Transform(start_pos_player, 1.0f, 0.0f), "Player");
+            player.AddComponent(new Collider(Vector2.Zero, start_pos_player, 1f, 1f));
 
-            enemy.AddComponent(new Collider(Vector2.Zero, start_pos_enemy, 0.5f, 0.5f));
-            player.AddComponent(new Collider(Vector2.Zero, start_pos_player, 0.5f, 0.5f));
-
-            collisionSystem.Entities.Add(enemy);
             collisionSystem.Entities.Add(player);
+            collisionSystem.Entities.Add(enemy);
+
+            enemy.GetComponent<Transform>().Position -= new Vector2(2, 0);
+            player.GetComponent<Transform>().Position += new Vector2(2, 0);
 
             collisionSystem.Update(0);
-            enemy.GetComponent<Transform>().Position += new Vector2(-1, 0);
-            player.GetComponent<Transform>().Position += new Vector2(1, 0);
-            collisionSystem.Update(0);
 
-            Assert.AreEqual(start_pos_enemy, enemy.GetComponent<Transform>().Position);
+            Assert.AreEqual(start_pos_player, player.GetComponent<Transform>().Position);
         }
 
+        /*
+            Explanation for how the CollidesCleanedEntity function operates and how to test it.
+
+            W = Wall | P = Player | E = Enemy
+
+            WW PP EE
+            WW PP EE
+
+            player and enemy are moving:
+
+            WP EE
+            WP EE
+
+            player is inside the wall
+                -> collision detection set the player to his last position:
+
+            WW PE
+            WW PE
+
+            The last known position for the player is where the enemy is now
+                -> another Collision: set the enemy to his last known position
+        */
+
         [TestMethod]
-        public void CleanCollision()
+        public void CleanedCollision()
         {
-            // Test when both player and enemy are going to the same point one of them should be set back to his start position
             TypeRegistry.Init();
             CollisionSystem collisionSystem = new ("collisionSystem");
 
-            IEntity wall = new Entity(new Transform(new Vector2(0, 0), 1f, 0f), "Wall");
-            wall.AddComponent(new Collider(new Vector2(0, 0), 1f, 10f));
+            // Create all entities
+            Entity wall = new Entity(new Transform(new Vector2(0, 0), 1f, 0f), "Wall");
+            wall.AddComponent(new Collider(new Vector2(0, 0), 1f, 2));
 
-            IEntity player = new Entity(new Transform(new Vector2(1f, 0), 1f, 0f), "Player");
+            Entity player = new Entity(new Transform(new Vector2(1f, 0), 1f, 0f), "Player");
             player.AddComponent(new Collider(new Vector2(1f, 0), 1f, 1f));
 
-            IEntity enemy = new Entity(new Transform(new Vector2(2f, 0), 1f, 0f), "Enemy");
+            Entity enemy = new Entity(new Transform(new Vector2(2f, 0), 1f, 0f), "Enemy");
             enemy.AddComponent(new Collider(new Vector2(2f, 0), 1f, 1f));
 
+            // Add entities to the collisionsystem
             collisionSystem.Entities.Add(wall);
             collisionSystem.Entities.Add(enemy);
             collisionSystem.Entities.Add(player);
@@ -149,7 +317,6 @@ namespace VilleonTests
 
             collisionSystem.Update(0);
 
-            Assert.AreEqual(1f, player.GetComponent<Transform>().Position.X);
             Assert.AreEqual(2f, enemy.GetComponent<Transform>().Position.X);
         }
     }
