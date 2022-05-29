f19c0a6b1b837c88b7e357f52aba7f60cee63c7f
weidner <henrik.weidner@hs-weingarten.de>
2022-05-29 21:16:33 +0200

    Added Village Tilemap

---
+++ Villeon/Villeon/Assets/TileMap/Tilesets/Village.tsx
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Village" tilewidth="32" tileheight="32" tilecount="520" columns="26">
+ <image source="Village.png" trans="8d8d8d" width="832" height="640"/>
+ <wangsets>
+  <wangset name="Dirrt" type="corner" tile="289">
+   <wangcolor name="Dirt" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Grass" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="165" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="166" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="167" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="184" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="185" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="191" wangid="0,2,0,2,0,0,0,0"/>
+   <wangtile tileid="192" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="193" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="209" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="210" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="211" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="212" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="217" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="218" wangid="0,2,0,0,0,0,0,2"/>
+   <wangtile tileid="219" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="236" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="237" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="242" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="243" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="244" wangid="0,2,0,0,0,2,0,2"/>
+   <wangtile tileid="245" wangid="0,2,0,2,0,0,0,2"/>
+   <wangtile tileid="268" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="269" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="270" wangid="0,0,0,2,0,2,0,2"/>
+   <wangtile tileid="271" wangid="0,2,0,2,0,2,0,0"/>
+   <wangtile tileid="287" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="288" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="313" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="314" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="457" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="458" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="459" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="483" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="484" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="485" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="509" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="510" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="511" wangid="0,1,0,1,0,1,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
