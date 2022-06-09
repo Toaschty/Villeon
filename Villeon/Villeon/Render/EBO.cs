2c0700ab37188d42fa8502e93fdb651e3ccd74a4
weidner <henrik.weidner@hs-weingarten.de>
2022-06-09 16:49:26 +0200

    Added VAO, EBO, VAO Added Depthtest

---
+++ Villeon/Villeon/Render/EBO.cs
@@ -0,0 +1,33 @@
+﻿using OpenTK.Graphics.OpenGL;
+
+namespace Villeon.Render
+{
+    public class EBO
+    {
+        private int _eboID = 0;
+
+        public EBO(int[] indices, int sizeInBytes)
+        {
+            GL.GenBuffers(1, out _eboID);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _eboID);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, sizeInBytes, indices, BufferUsageHint.StaticDraw);
+        }
+
+        public int ID { get => _eboID; }
+
+        public void Bind()
+        {
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _eboID);
+        }
+
+        public void Unbind()
+        {
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+        }
+
+        public void Delete()
+        {
+            GL.DeleteBuffers(1, ref _eboID);
+        }
+    }
+}
---
+++ Villeon/Villeon/Render/VBO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL;
+
+namespace Villeon.Render
+{
+    public class VBO
+    {
+        private int _vboID = 0;
+
+        public VBO(int sizeInBytes)
+        {
+            GL.GenBuffers(1, out _vboID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vboID);
+            GL.BufferData(BufferTarget.ArrayBuffer, sizeInBytes, (IntPtr)null, BufferUsageHint.StaticDraw);
+        }
+
+        public int ID { get => _vboID; }
+
+        public void Bind()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vboID);
+        }
+
+        public void SetData(float[] vertices, int sizeInBytes)
+        {
+            GL.BufferSubData(BufferTarget.ArrayBuffer, (IntPtr)0, sizeInBytes, vertices);
+        }
+
+        public void Unbind()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        }
+
+        public void Delete()
+        {
+            GL.DeleteBuffers(1, ref _vboID);
+        }
+    }
+}
