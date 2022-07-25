1d12b980b568678b81b842be954b28b428d9faa1
Pucknat <Marvin.pucknat@hs-weingarten.de>
2022-07-25 12:11:53 +0200

    Fixed wrong file names

--- Villeon/Villeon/Assets/Asset.cs
+++ Villeon/Villeon/Assets/Asset.cs
@@ -94,7 +94,7 @@ namespace Villeon.Assets
             AddSpriteSheet("Animations.slime_jumping.png", new SpriteSheet(Color4.White, GetTexture("Animations.slime_jumping.png"), 32, 39, 13, 0, SpriteLayer.Foreground));
             AddSpriteSheet("Animations.slime_attack_left.png", new SpriteSheet(Color4.White, GetTexture("Animations.slime_attack_left.png"), 32, 32, 4, 0, SpriteLayer.Foreground));
             AddSpriteSheet("Animations.slime_attack_right.png", new SpriteSheet(Color4.White, GetTexture("Animations.slime_attack_right.png"), 32, 32, 4, 0, SpriteLayer.Foreground));
-            AddSpriteSheet("Enemies.Slime.slime_jumping.png", new SpriteSheet(Color4.White, GetTexture("Enemies.Slime.slime_jumping.png"), 32, 39, 13, 0, SpriteLayer.Foreground));
+            AddSpriteSheet("Enemies.slime.slime_jumping.png", new SpriteSheet(Color4.White, GetTexture("Enemies.slime.slime_jumping.png"), 32, 39, 13, 0, SpriteLayer.Foreground));
             AddSpriteSheet("Enemies.Bat.bat_left.png", new SpriteSheet(Color4.White, GetTexture("Enemies.Bat.bat_left.png"), 32, 32, 4, 0, SpriteLayer.Foreground));
             AddSpriteSheet("Enemies.Bat.bat_right.png", new SpriteSheet(Color4.White, GetTexture("Enemies.Bat.bat_right.png"), 32, 32, 4, 0, SpriteLayer.Foreground));
             AddSpriteSheet("Enemies.Bubble.bubble_left.png", new SpriteSheet(Color4.White, GetTexture("Enemies.Bubble.bubble_left.png"), 45, 45, 8, 0, SpriteLayer.Foreground));
